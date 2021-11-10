0cee9cd4a4fab115a48454b544ef749273ddc414
Rhea <Rheaazarrag@gmail.com>
2021-11-09 17:57:07 -0800

    Import classNames and apply conditional logic for classes

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,8 +1,19 @@
 import React from "react";
+import "./DayListItem.scss";
+import classNames from "classnames";
 
-export default function DayListItem(props) {
+export default function DayListItem (props) {
+
+  const dayClass = classNames("day-listitem", {
+    "day-listitem--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  });
+
+
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li 
+      className={dayClass} 
+      onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{props.spots}</h3>
     </li>
