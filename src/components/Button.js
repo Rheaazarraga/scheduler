a1571a3cad469fdfc54f570e74c30d9b9925cc6c
Rhea <Rheaazarrag@gmail.com>
2021-11-09 18:49:40 -0800

    Section off code for code readability

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,18 +2,21 @@ import React from "react";
 import classNames from "classnames";
 import "components/Button.scss";
 
+// --------------- Component function --------------- //
+
 export default function Button(props) {
   const buttonClass = classNames("button", {
     "button--confirm": props.confirm,
     "button--danger": props.danger
   });
 
+// --------------- Button component --------------- //
+
   return (
     <button
       className={buttonClass}
       onClick={props.onClick}
-      disabled={props.disabled}
-    >
+      disabled={props.disabled}>
       {props.children}
     </button>
   );
