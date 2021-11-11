f079a742ac2235b8caf7cd9758be51e80d82449a
Rhea <Rheaazarraga@gmail.com>
2021-11-10 16:16:31 -0800

    Refactor InterviewerList using map to dynamically generate and render the interviewers

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./InterviewerList.scss"
+import InterviewerListItem from "./InterviewerListItem";
+
+// --------------- Component Function --------------- //
+
+export default function InterviewerList(props) {
+
+  const interviewers = props.interviewers.map(interviewer => {
+
+    return (
+        <InterviewerListItem
+          key={interviewer.id}
+          name={interviewer.name}
+          avatar={interviewer.avatar}
+          selected={interviewer.id === props.interviewer}
+          setInterviewer={() => props.setInterviewer(interviewer.id)}
+        />
+    )
+  });
+  console.log(interviewers);
+
+// --------------- InterviewList component --------------- //
+
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">interviewer</h4>
+      <ul className="interviewers__list">{interviewers}</ul>
+    </section>
+
+  );
+}
