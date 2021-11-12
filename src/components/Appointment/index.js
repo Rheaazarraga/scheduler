bed349063ed08876c07b76b7504e42ceaf114522
Rhea <Rheaazarraga@gmail.com>
2021-11-12 14:00:44 -0800

    import classnames, implement conditional rendering for appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,15 +1,29 @@
 import React from "react";
 import "./styles.scss";
+import classNames from "classnames";
 
 // --------------- Component Function --------------- //
 
 export default function Appointment(props) {
+
+  const appointmentClass = classNames("appointment", {
+    "appointment__time":props.selected
+  });
+
+  const appointments = (time) => {
+    if (time === undefined) {
+      return "No appointments"
+    }
+    return `Appointment at ${time} `;
+  }
 
 
   // --------------- Appointment component --------------- //
   
   return (
 
-    <article className="appointment"></article>
+    <article className={appointmentClass}
+      className="appointment__time">{appointments(props.time)}
+    </article>
   );
 } 
