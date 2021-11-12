7c92c8725e4442b19254c5ca8a7a56825d079e7e
Rhea <Rheaazarraga@gmail.com>
2021-11-12 12:11:35 -0800

    Create Appointment component

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// --------------- Component Function --------------- //
+
+export default function Appointment(props) {
+
+
+  // --------------- Appointment component --------------- //
+  
+  return (
+
+    <article className="appointment"></article>
+  );
+} 
