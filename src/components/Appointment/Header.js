1cb0a480f57d6b4483aa744670b3d6a78902b173
Rhea <Rheaazarraga@gmail.com>
2021-11-12 15:51:43 -0800

    Create Header component

---
+++ src/components/Appointment/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+// --------------- Component Function --------------- //
+export default function Header(props) {
+
+
+// --------------- Header component --------------- //
+  return (
+    <header className="appointment__time">
+      <h4 className="text--semi-bold">12pm</h4>
+      <hr className="appointment__separator" />
+    </header>
+  );
+}
