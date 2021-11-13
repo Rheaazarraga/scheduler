643895154c038176a6675e80c0ee88abb54ce4b6
Rhea <Rheaazarraga@gmail.com>
2021-11-12 21:53:51 -0800

    Correct import naming convention, add story logic for Show component

--- stories/index.js
+++ stories/index.js
@@ -9,10 +9,10 @@ import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
-import Appointment from "components/Appointment/index.js";
-import Header from "components/Appointment/Header.js";
-import Empty from "components/Appointment/Empty.js";
-import Show from "components/Appointment/Show.js";
+import Appointment from "components/Appointment/index";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
 // --------------- Stories --------------- //
 
@@ -152,4 +152,9 @@ storiesOf("Appointment", module)
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
    .add("Header", () => <Header time="12pm" />)
-   .add("Empty", () => <Empty onAdd={action("onAdd")} />);
+   .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+   .add("Show", () => <Show 
+                      student="Lydia Miller-Jones"
+                      interviewer={interviewer}
+                      onEdit={action("onEdit")}
+                      onDelete={action("onDelete")} />);
