10de0b03ea92f69b6561d5e2d549b9feb3128df5
Rhea <Rheaazarraga@gmail.com>
2021-11-12 16:10:10 -0800

    Create Empty view component

---
+++ src/components/Appointment/Empty.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+// --------------- Component Function --------------- //
+
+export default function Empty(props) {
+
+  // --------------- Empty component --------------- //
+  return (
+    <main className="appointment__add">
+      <img
+      className="appointment__add-button"
+      src="images/add.png"
+      alt="Add"
+      />
+    </main>
+  );
+}
