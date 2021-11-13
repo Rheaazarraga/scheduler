c4cbf17b630a730a2fe03536bc5923d21214d241
Rhea <Rheaazarraga@gmail.com>
2021-11-12 22:17:51 -0800

    Create Status component

---
+++ src/components/Appointment/Status.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+// --------------- Component Function --------------- //
+
+export default function Status(props) {
+
+  // --------------- Status component --------------- //
+
+  return(
+    <main className="appointment__card appointment__card--status">
+      <img
+        className="appointment__status-image"
+        src="images/status.png"
+        alt="Loading"
+      />
+      <h1 className="text--semi-bold">Deleting</h1>
+    </main>
+
+  )
+}
