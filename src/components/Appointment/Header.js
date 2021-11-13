f6c5dcd20ba74e95e10b4965ce960f4f16d31e65
Rhea <Rheaazarraga@gmail.com>
2021-11-12 16:00:31 -0800

    Update component to display the time props being passed to it

--- src/components/Appointment/Header.js
+++ src/components/Appointment/Header.js
@@ -1,14 +1,14 @@
 import React from "react";
-
+import classNames from "classnames";
 
 // --------------- Component Function --------------- //
 export default function Header(props) {
-
+  
 
 // --------------- Header component --------------- //
   return (
     <header className="appointment__time">
-      <h4 className="text--semi-bold">12pm</h4>
+      <h4 className="text--semi-bold">{props.time}</h4>
       <hr className="appointment__separator" />
     </header>
   );
