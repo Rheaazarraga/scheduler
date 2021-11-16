520f526d919c9f51beb7fce0e3b939ad0f32724b
Rhea <Rheaazarraga@gmail.com>
2021-11-16 15:47:16 -0800

    Update to display props from stories, use input tags to create controlled list, and add reset and cancel functions to reflect useState

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from 'react';
 import InterviewerList from "components/InterviewerList";
 import Button from "components/Button";
 
@@ -6,6 +6,20 @@ import Button from "components/Button";
 
 export default function Form(props) {
 
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = () => {
+    setStudent("")
+    setInterviewer(null)
+  };
+
+  const cancel = () => {
+    reset()
+    props.onCancel()
+  }
+
+   // --------------- Form component --------------- //
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -15,20 +29,20 @@ export default function Form(props) {
             name="name"
             type="text"
             placeholder="Enter Student Name"
-            /*
-              This must be a controlled component
-              your code goes here
-            */
+            value={student}
+            onChange={(e) => setStudent(e.target.value)}
           />
         </form>
         <InterviewerList 
-          /* your code goes here */
+          interviewers={props.interviewers}
+          value={interviewer}
+          onChange={setInterviewer}
         />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger {/* your code goes here */}>Cancel</Button>
-          <Button confirm {/* your code goes here */}>Save</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
+          <Button confirm onClick={props.onSave}>Save</Button>
         </section>
       </section>
     </main>
