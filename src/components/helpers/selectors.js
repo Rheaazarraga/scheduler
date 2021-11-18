ca65068c83d14b87d1f96c798ca3f0c483d11726
Rhea <Rheaazarraga@gmail.com>
2021-11-18 14:17:10 -0800

    Add logic to getAppointentsForDay to pass unit tests

--- src/components/helpers/selectors.js
+++ src/components/helpers/selectors.js
@@ -1,6 +1,15 @@
 import React from "react";
 
-
 export function getAppointmentsForDay(state, day) {
+  const filteredAppointments = state.days.filter(days => days.name === day);
+  const appointmentsArray = [];
 
+  if (filteredAppointments[0]) {
+    const dayIds = filteredAppointments[0].appointments
+
+    for (const numAppoint of dayIds) {
+      appointmentsArray.push(state.appointments[numAppoint]);
+    }
+  }
+  return appointmentsArray;
 }
