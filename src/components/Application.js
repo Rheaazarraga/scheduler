05f5091c4b2a9e586b63f9af9e158f1c070ba50f
Rhea <Rheaazarraga@gmail.com>
2021-11-22 13:33:29 -0800

    Implement promise.all and dailyAppointments to retrieve and display appointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,10 +3,10 @@ import DayList from "./DayList";
 import "components/Application.scss";
 import Appointment from "./Appointment";
 import axios from "axios";
+import { getAppointmentsForDay } from "./helpers/selectors";
 
 export default function Application(props) {
 
-  
   const [state, setState] = useState({
     day: "Monday",
     days: [],
@@ -14,20 +14,23 @@ export default function Application(props) {
   });
   
   const setDay = day => setState({ ...state, day });
-  
-  const setDays = (days) => setState(prev => ({ ...prev, days }));
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
 
   useEffect(() => {
-    axios.get(`http://localhost:8001/api/days`).then(response => {
-      setDays([...response.data]);
-    });
+      Promise.all([
+        axios.get(`http://localhost:8001/api/days`),
+        axios.get(`http://localhost:8001/api/appointments`)
+      ]).then((all) => {
+        setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}));
+      })
   }, []);
 
-  const appointmentsArray = Object.values(state.appointments).map(appointment => {
+
+  const appointmentsArray = Object.values(dailyAppointments).map((appointment) => {
     return (
       <Appointment
-      key={appointment.id}
-      {...appointment}
+        key={appointment.id}
+        {...appointment}
       />
     );
   });
