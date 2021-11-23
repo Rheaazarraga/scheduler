0a36eb2b4a6acfe3d30d556e90e8f729db6af783
Rhea <Rheaazarraga@gmail.com>
2021-11-22 22:29:09 -0800

    Add logic for transition function

--- src/components/hooks/useVisualMode.js
+++ src/components/hooks/useVisualMode.js
@@ -1,8 +1,14 @@
 import React, { useState } from "react";
 
+// --------------- useVisualMode --------------- //
+
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  
+  function transition(mode) {
+    setMode(mode);
+  }
 
-  return { mode };
+  return { mode, transition };
 
 }
