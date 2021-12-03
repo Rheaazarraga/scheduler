333db74f0bd193c605d5f8cf885efcf7751864d3
Rhea <Rheaazarraga@gmail.com>
2021-12-02 21:01:41 -0800

    Add error state into Form, add validate function within button component to return error message if one is present, before saving

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -8,6 +8,7 @@ export default function Form(props) {
 
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
   const reset = () => {
     setStudent("")
@@ -19,21 +20,23 @@ export default function Form(props) {
     props.onCancel()
   };
 
-   // --------------- Form component --------------- //
+  // --------------- Form component --------------- //
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
         <form onSubmit={e => e.preventDefault()} autoComplete="off">
           <input
+            data-testid="student-name-input"
             className="appointment__create-input text--semi-bold"
-            name="name"
+            name="student"
             type="text"
             placeholder="Enter Student Name"
             value={student}
             onChange={(e) => setStudent(e.target.value)}
           />
+          <section className="appointment__validation">{error}</section>
         </form>
-        <InterviewerList 
+        <InterviewerList
           interviewers={props.interviewers}
           value={interviewer}
           onChange={setInterviewer}
@@ -42,7 +45,13 @@ export default function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={() => {props.onSave(student, interviewer)}}>Save</Button>
+          <Button confirm onClick={() => {
+            if (student === "") {
+              setError("Student name cannot be blank");
+              return;
+            }
+            props.onSave(student, interviewer)
+          }}>Save</Button>
         </section>
       </section>
     </main>
