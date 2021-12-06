b1c6d0ad3113e0e6ae79a58f8b95acf954bc3147
Rhea <Rheaazarraga@gmail.com>
2021-12-05 16:44:40 -0800

    Fix import

--- src/components/hooks/__tests__/useVisualMode.test.js
+++ src/components/hooks/__tests__/useVisualMode.test.js
@@ -1,6 +1,6 @@
 import { renderHook, act } from "@testing-library/react-hooks";
 
-import useVisualMode from "./useVisualMode.js";
+import useVisualMode from "../useVisualMode.js";
 
 const FIRST = "FIRST";
 const SECOND = " SECOND";
