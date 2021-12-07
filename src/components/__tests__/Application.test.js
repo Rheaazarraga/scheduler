3074c5c419631dacb0ce25c192ed3e72ffa06784
Rhea <Rheaazarraga@gmail.com>
2021-12-06 17:19:50 -0800

    Convert the promise chain in application test to async await syntax- defining asynch function with keyword and hiding the promise chain with await keyword

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -8,9 +8,11 @@ afterEach(cleanup);
 
 it("defaults to Monday and changes the schedule when a new day is selected", async() => {
   const { getByText } = render(<Application />);
+  
+//The Promise chain can be hidden by using the await keyword
+  await waitForElement(() => getByText("Monday"));
 
-  return waitForElement(() => getByText("Monday")).then(() => {
-    fireEvent.click(getByText("Tuesday"));
-    expect(getByText("Leopold Silvers")).toBeInTheDocument();
-  });
+  fireEvent.click(getByText("Tuesday"));
+
+  expect(getByText("Leopold Silvers")).toBeInTheDocument();
 });
