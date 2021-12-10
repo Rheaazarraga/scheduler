1655a3a1671209f0a50a2cf8263595016b7a98d6
Rhea <Rheaazarraga@gmail.com>
2021-12-09 21:53:40 -0800

    Fix import path from file restructuring

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,8 +2,8 @@ import React from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
-import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "./helpers/selectors";
-import useApplicationData from "./hooks/useApplicationData";
+import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
+import useApplicationData from "hooks/useApplicationData";
 
 export default function Application(props) {
 
