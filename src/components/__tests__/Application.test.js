b3645758bb04830f2e9a8de79bbdeb6a0cae5198
Rhea <Rheaazarraga@gmail.com>
2021-12-09 17:36:38 -0800

    Import prettyDOM from testing-library/react

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent, getByText } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM } from "@testing-library/react";
 
 import Application from "components/Application";
 
@@ -21,7 +21,7 @@ it("defaults to Monday and changes the schedule when a new day is selected", asy
 
 it("loads data, books an interview and reduces the spots remaining for the first day by 1", () => {
   const { container } = render(<Application />);
-  console.log(container);
+  console.log(prettyDOM(container));
 });
 
 });
