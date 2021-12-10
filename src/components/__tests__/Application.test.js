6a7bf708e7be35f3bfee22895c28f25f7df8160f
Rhea <Rheaazarraga@gmail.com>
2021-12-09 18:13:00 -0800

    Import additional queries from react-testng-library, add logic to trigger 3 click events and 1 change event

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM, getAllByTestId } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM, getAllByTestId, getAltText, getByPlaceholderText, getByAltText } from "@testing-library/react";
 
 import Application from "components/Application";
 
@@ -28,7 +28,16 @@ it("loads data, books an interview and reduces the spots remaining for the first
 
   const appointment = getAllByTestId(container, "appointment")[0];
 
-  console.log(prettyDOM(appointment));
+  fireEvent.click(getByAltText(appointment, "Add"));
+
+  fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+    target: { value: "Lydia Miller-Jones" }
+  });
+  fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+
+  fireEvent.click(getByText(appointment, "Save"));
+
+
 });
 
 });
