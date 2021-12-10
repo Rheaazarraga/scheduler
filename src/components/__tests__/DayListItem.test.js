78b0caec50d33d6d7caf1b4412424b81e5a4c385
Rhea <Rheaazarraga@gmail.com>
2021-12-09 21:43:53 -0800

    Add comments of next 4 tests to implement

--- src/components/__tests__/DayListItem.test.js
+++ src/components/__tests__/DayListItem.test.js
@@ -21,3 +21,14 @@ it("renders '2 spots remaining' when there are 2 spots", () => {
   const { getByText } = render(<DayListItem name="Monday" spots={2} />);
   expect(getByText("2 spots remaining")).toBeInTheDocument();
 });
+
+//"loads data, cancels an interview and increases the spots remaining for Monday by 1"
+
+
+// "loads data, edits an interview and keeps the spots remaining for Monday the same"
+
+
+// "shows the save error when failing to save an appointment"
+
+
+// "shows the delete error when failing to delete an existing appointment"
