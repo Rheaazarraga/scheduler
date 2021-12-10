0d660b83844ed464e54318dedd5215de7654e046
Rhea <Rheaazarraga@gmail.com>
2021-12-09 18:47:14 -0800

    Add comments as a guideline to follow for test planning the mock data

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -20,24 +20,41 @@ it("defaults to Monday and changes the schedule when a new day is selected", asy
 });
 
 it("loads data, books an interview and reduces the spots remaining for the first day by 1", async() => {
+  // render the application
   const { container } = render(<Application />);
 
+  // wait until the text "Archie Cohen is displayed"
   await waitForElement(() => getByText(container, "Archie Cohen"));
 
+  // get all the appointment articles
   const appointments = getAllByTestId(container, "appointment");
 
+  // retrieves the first appointment article which is an empty appointment from the mock data
   const appointment = appointments[0];
 
+  // click the "Add" button on the first empty appointment
   fireEvent.click(getByAltText(appointment, "Add"));
 
+  //Enter the name "Lydia MIller-Jones" into the input with the placeholder "Enter Student Name"
   fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
     target: { value: "Lydia Miller-Jones" }
   });
+  // click the first interviewer in the list
   fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
-
+  // click the "Save" button on that same appointment.
   fireEvent.click(getByText(appointment, "Save"));
 
   console.log(prettyDOM(appointment));
 });
+
+
+
+// --------------- More test plans --------------- //
+
+// check that the element with the text "Saving" is displayed
+
+// wait until the element with the text "Lydia Miller-Jones" is displayed
+
+// check that the DayListItem with the text "Monday" also has the text "no spots remaining"
 
 });
