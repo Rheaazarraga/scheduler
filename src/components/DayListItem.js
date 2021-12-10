5f590bded588e3f43e599f26a765782b36311622
Rhea <Rheaazarraga@gmail.com>
2021-12-09 20:45:47 -0800

    Add data-testid to root <li> element

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -27,8 +27,8 @@ export default function DayListItem (props) {
 
   return (
     <li 
-      className={dayClass} 
-      onClick={() => props.setDay(props.setDay)}>
+      className={dayClass}
+      onClick={() => props.setDay(props.setDay)} data-testid="day">
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
